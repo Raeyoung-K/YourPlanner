804fb32de70837270fdbdaea041af9fdb2d853a3
Raeyoung Kim <kimrae@sheridancollege.ca>
2023-09-12 10:56:42 -0400

    Initial Commit

---
+++ YourPlanner/YourPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourPlannerApp.swift
+//  YourPlanner
+//
+//  Created by Raeyoung Kim on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct YourPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
